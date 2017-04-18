deb9e068bc413b92758ff74174075d393c6aab8b
Emil Bjerglund Pedersen <emilbp@gmail.com>
2017-04-18 18:02:36 +0200

    vca function working

--- R/vca.R
+++ R/vca.R
@@ -5,7 +5,7 @@ vca_testdata <- function(N = 100, L = 200) {
   gr_lorentzian <- function(x, G, x_0, A) {
     A * (1/pi) * (0.5 * G) / ((x - x_0)^2 + (0.5 * G)^2)
   }
-  fun1 <- function(p) p*gr_lorentzian(w, 1, 5, 1) + (1-p)*gr_lorentzian(w, 1.5, 12, 1) + rnorm(n = length(w), mean = 0.05, sd = 0.02)
+  fun1 <- function(p) p*gr_lorentzian(w, 1, 5, 1) + (1-p)*gr_lorentzian(w, 1.5, 12, 1) + rnorm(n = length(w), mean = 0.01, sd = 0.005)
 
   testdata <- matrix(nrow = length(w), ncol = n)
   for(x in seq_along(ps)) {
@@ -14,64 +14,89 @@ vca_testdata <- function(N = 100, L = 200) {
   list(N = N, L = L, R = testdata, w = w)
 }
 
-R_test <- vca_testdata(N = 100, L = 200)
+vca <- function(R, p) {
+  #p <- 2 # endmembers
+  L = dim(R)[1]
+  N = dim(R)[2]
+
+  r_m <- cbind(rowMeans(R))
+  R_m <- matrix(data = rep(r_m, N), ncol = N, byrow = FALSE)
+  R_o <- R - R_m
+  Ud <- svd((R_o %*% t(R_o))/N,p)$u
+
+  x_p <- t(Ud) %*% R_o
 
 
+  SNR <- snr_est(R, r_m, x_p)
+  SNR_th <- 15 + 10*log10(p)
 
-p <- 2 # endmembers
-r_m <- cbind(rowMeans(R_test$R))
-R_m <- matrix(data = rep(r_m, R_test$N), ncol = R_test$N, byrow = FALSE)
-R_o <- R_test$R - R_m
-Ud <- svd((R_o %*% t(R_o))/R_test$N,p)$u
+  if (SNR > SNR_th) {
+    print("Projective projection, U from SVD", quote = FALSE)
+    d <- p
+    Ud <- svd((R %*% t(R)) / N, d)$u
+    x_p <- t(Ud) %*% R
+    Rp <- Ud %*% x_p[1:d,]
+    x <- t(Ud) %*% R
+    u <- rowMeans(x)
+    y <- x / matrix(colSums(x * matrix(u, ncol = N, nrow = p)), ncol = N, nrow = d, byrow = TRUE)
+  } else {
+    d <- p - 1
+    print("Projection to p-1 subspace, U from PCA", quote = FALSE)
 
-x_p <- t(Ud) %*% R_o
+    Ud <- cbind(Ud[,1:d])
+
+    Rp <- Ud %*% x_p[1:d,] + R_m
+    x <- x_p[1:d,]
+    cx <- sqrt(max(colSums(as.matrix(x)^2)))
+    y <- rbind(x, cx * rep(1, N))
+  }
 
 
-SNR <- snr_est(R_test$R, r_m, x_p)
-SNR_th <- 15 + 10*log10(p)
+  # VCA algorithm
+  indice <- matrix(data = rep(0, p), ncol = p)
+  A <- matrix(data = rep(0, p^2), ncol = p)
+  A[p,1] <- 1
 
-if (SNR > SNR_th) {
-  print("Projective projection, U from SVD", quote = FALSE)
-  d <- p
-  Ud <- svd((R_test$R %*% t(R_test$R)) / R_test$N, d)$u
-  x_p <- t(Ud) %*% R_test$R
-  Rp <- Ud %*% x_p[1:d,]
-  x <- t(Ud) %*% R_test$R
-  u <- rowMeans(x)
-  y <- x / matrix(colSums(x * matrix(u, ncol = R_test$N, nrow = p)), ncol = R_test$N, nrow = d, byrow = TRUE)
-} else {
-  d <- p - 1
-  print("Projection to p-1 subspace, U from PCA", quote = FALSE)
+  for (i in 1:p) {
+    w <- runif(n = p)
+    f <- w - A %*% pracma::pinv(A) %*% w
+    f <- f / sqrt(sum(f^2))
 
-  Ud <- cbind(Ud[,1:d])
+    v <- t(f) %*% y
+    indice[i] <- which(abs(v) == max(abs(v)))
+    A[, i] <- y[, indice[i]]
+  }
 
-  Rp <- Ud %*% x_p[1:d,] + R_m
-  x <- x_p[1:d,]
-  cx <- sqrt(max(colSums(as.matrix(x)^2)))
-  y <- rbind(x, cx * rep(1, R_test$N))
+  Ae = Rp[, indice]
+
+  list(A = Ae, Rp = Rp, ind = indice)
 }
 
+R_test <- vca_testdata(N = 10, L = 200)
+R_test.vca <- vca(R_test$R, 2)
 
-# VCA algorithm
-indice <- matrix(data = rep(0, p), ncol = p)
-A <- matrix(data = rep(0, p^2), ncol = p)
-A[p,1] <- 1
 
-for (i in 1:p) {
-  w <- runif(n = p)
-  f <- w - A %*% pracma::pinv(A) %*% w
-  f <- f / sqrt(sum(f^2))
+# Estimated mixing of components
+s_est <- t(pracma::pinv(R_test.vca$A) %*% R_test.vca$Rp)
 
-  v <- t(f) %*% y
-  indice[i] <- which(abs(v) == max(abs(v)))
-  A[, i] <- y[, indice[i]]
-}
+print(round(s_est,2))
 
-Ae = Rp[, indice]
 
-plot(R_test$w, Ae[,1], type = 'l', col = 'red')
-lines(R_test$w, Ae[,2], type = 'l', col = 'blue')
 
+
+library(tidyverse)
+
+df <- bind_cols(tibble(w = R_test$w, `Spec 1 data` = R_test$R[,1], `Spec 1 udledt` = s_est[1,1] * R_test.vca$A[,1] + s_est[1,2] * R_test.vca$A[,2]), R_test.vca$A %>% as_tibble())
+
+g1 <- df %>% select(w, V1, V2) %>% gather(key = 'VC', value = 'int', -w) %>%
+  ggplot(aes(x = w, y = int, group = VC, color = VC)) +
+  geom_path()
+
+g2 <- df %>% select(w, starts_with("Spec")) %>% gather(key = 'VC', value = 'int', -w) %>%
+  ggplot(aes(x = w, y = int, group = VC, color = VC)) +
+  geom_path()
+
+gridExtra::grid.arrange(g1, g2)
 #
 # library(tidyverse)
 # R_test %>%
